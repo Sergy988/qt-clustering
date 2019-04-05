becc6ea7acf5ef4d16be6b7fc68c04e0e09bfceb
loreloc <lorenzoloconte@outlook.it>
2019-04-05 13:41:08 +0200

    simplified training set data initialization

--- src/Data.java
+++ src/Data.java
@@ -8,20 +8,20 @@ public class Data {
 	Data() {
 		data = new Object[14][5];
 
-		data[ 0][0] = "sunny";    data[ 0][1] = "hot";  data[ 0][2] = "high";   data[ 0][3] = "weak";   data[ 0][4] = "no";
-		data[ 1][0] = "sunny";    data[ 1][1] = "hot";  data[ 1][2] = "high";   data[ 1][3] = "strong"; data[ 1][4] = "no";
-		data[ 2][0] = "overcast"; data[ 2][1] = "hot";  data[ 2][2] = "high";   data[ 2][3] = "weak";   data[ 2][4] = "yes";
-		data[ 3][0] = "rain";     data[ 3][1] = "mild"; data[ 3][2] = "high";   data[ 3][3] = "weak";   data[ 3][4] = "yes";
-		data[ 4][0] = "rain";     data[ 4][1] = "cool"; data[ 4][2] = "normal"; data[ 4][3] = "weak";   data[ 4][4] = "yes";
-		data[ 5][0] = "rain";     data[ 5][1] = "cool"; data[ 5][2] = "normal"; data[ 5][3] = "strong"; data[ 5][4] = "no";
-		data[ 6][0] = "overcast"; data[ 6][1] = "cool"; data[ 6][2] = "normal"; data[ 6][3] = "strong"; data[ 6][4] = "yes";
-		data[ 7][0] = "sunny";    data[ 7][1] = "mild"; data[ 7][2] = "high";   data[ 7][3] = "weak";   data[ 7][4] = "no";
-		data[ 8][0] = "sunny";    data[ 8][1] = "cool"; data[ 8][2] = "normal"; data[ 8][3] = "weak";   data[ 8][4] = "yes";
-		data[ 9][0] = "rain";     data[ 9][1] = "mild"; data[ 9][2] = "normal"; data[ 9][3] = "weak";   data[ 9][4] = "yes";
-		data[10][0] = "sunny";    data[10][1] = "mild"; data[10][2] = "normal"; data[10][3] = "strong"; data[10][4] = "yes";
-		data[11][0] = "overcast"; data[11][1] = "mild"; data[11][2] = "high";   data[11][3] = "strong"; data[11][4] = "yes";
-		data[12][0] = "overcast"; data[12][1] = "hot";  data[12][2] = "normal"; data[12][3] = "weak";   data[12][4] = "yes";
-		data[13][0] = "rain";     data[13][1] = "mild"; data[13][2] = "high";   data[13][3] = "strong"; data[13][4] = "no";
+		data[ 0] = new String[]{ "sunny", "hot", "high", "weak", "no" };
+		data[ 1] = new String[]{ "sunny", "hot", "high", "strong", "no" };
+		data[ 2] = new String[]{ "overcast", "hot", "high", "weak", "yes" };
+		data[ 3] = new String[]{ "rain", "mild", "high", "weak", "yes" };
+		data[ 4] = new String[]{ "rain", "cool", "normal", "weak", "yes" };
+		data[ 5] = new String[]{ "rain", "cool", "normal", "strong", "no" };
+		data[ 6] = new String[]{ "overcast", "cool", "normal", "strong", "yes" };
+		data[ 7] = new String[]{ "sunny", "mild", "high", "weak", "no" };
+		data[ 8] = new String[]{ "sunny", "cool", "normal", "weak", "yes" };
+		data[ 9] = new String[]{ "rain", "mild", "normal", "weak", "yes" };
+		data[10] = new String[]{ "sunny", "mild", "normal", "strong", "yes" };
+		data[11] = new String[]{ "overcast", "mild", "high", "strong", "yes" };
+		data[12] = new String[]{ "overcast", "hot", "normal", "weak", "yes" };
+		data[13] = new String[]{ "rain", "mild", "high", "strong", "no" };
 
 		numberOfExamples = 14;
 
