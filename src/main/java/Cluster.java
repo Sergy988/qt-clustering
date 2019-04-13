95dfe82b5caf6eb41752e162f1d06c5b4fdf8180
loreloc <lorenzoloconte@outlook.it>
2019-04-13 23:57:00 +0200

    added documentation to Cluster.java

--- src/main/java/Cluster.java
+++ src/main/java/Cluster.java
@@ -1,39 +1,82 @@
 
+/**
+ * The Cluster class
+ */
 class Cluster {
 
+	/**
+	 * The centroid tuple of the cluster
+	 */
 	private Tuple centroid;
+
+	/**
+	 * The ids of the other tuple in the cluster
+	 */
 	private ArraySet clusteredData; 
 
+	/**
+	 * Instantiate a Cluster
+	 * @param centroid The centroid tuple
+	 */
 	public Cluster(Tuple centroid) {
 		this.centroid = centroid;
 		clusteredData = new ArraySet();
-		
 	}
 		
+	/**
+	 * Get the centroid tuple
+	 * @return The centroid tuple
+	 */
 	public Tuple getCentroid() {
 		return centroid;
 	}
 
+	/**
+	 * Add a tuple id to the cluster
+	 * @param id The tuple id
+	 * @return False if clustered data contains the id, otherwise true
+	 */
 	public boolean addData(int id) {
 		return clusteredData.add(id);
 	}
-	
+
+	/**
+	 * Check if the cluster contains a tuple id
+	 * @param id The tuple id
+	 * @return true if the cluster contains the tuple specified by the id
+	 */
 	public boolean contain(int id) {
 		return clusteredData.get(id);
 	}
 
+	/**
+	 * Remove a tuple id from the cluster
+	 * @param id The tuple id to remove
+	 */
 	public void removeTuple(int id) {
 		clusteredData.delete(id);
 	}
 	
+	/**
+	 * Get the size of the cluster
+	 * @return The size of the cluster
+	 */
 	public int getSize() {
 		return clusteredData.size();
 	}
 	
+	/**
+	 * Get an array of clustered tuple ids
+	 * @return An array that contains the clustered tuple ids
+	 */
 	public int[] iterator() {
 		return clusteredData.toArray();
 	}
 	
+	/**
+	 * Convert a Cluster to a String
+	 * @return The textual rappresentation of the Cluster
+	 */
 	public String toString() {
 		String str = "Centroid = ( ";
 
@@ -46,6 +89,11 @@ class Cluster {
 		return str;		
 	}
 	
+	/**
+	 * Convert a Cluster to a String based on source data
+	 * @param data The source data
+	 * @return The textual rappresentation of the Cluster
+	 */
 	public String toString(Data data) {
 		String str= "Centroid = ( ";
 
