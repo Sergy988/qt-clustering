91ca8ba6a20a9ac5fc2693d60a6fa24317e1dbcd
loreloc <lorenzoloconte@outlook.it>
2019-05-27 18:32:48 +0200

    fixed majorEigenvectors3D for correlation matrix with size equal 3

--- src/main/java/stats/MultiVariation.java
+++ src/main/java/stats/MultiVariation.java
@@ -30,7 +30,7 @@ public class MultiVariation {
 			throw new StatisticException("correlation matrix is not square!");
 		}
 
-		if (correlation.columns() < 3) {
+		if (correlation.columns() < 4) {
 			throw new StatisticException("correlation matrix is too small!");
 		}
 
